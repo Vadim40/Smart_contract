cb77deaf23555d1c4378d647a9d6f84b83bd4df6
Vadim40 <117930997+Vadim40@users.noreply.github.com>
2024-05-28 01:45:14 +0300

    Update Web3jConfig.java

--- src/main/java/com/example/sepolia/Web3jConfig.java
+++ src/main/java/com/example/sepolia/Web3jConfig.java
@@ -8,7 +8,7 @@ import org.web3j.protocol.http.HttpService;
 public class Web3jConfig {
     @Bean
     public Web3j web3j() {
-        return Web3j.build(new HttpService("http://localhost:8545")); // Проверьте, что узел Ethereum доступен на этом URL
+        return Web3j.build(new HttpService("http://localhost:8545")); 
     }
 
 
